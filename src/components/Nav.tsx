9298e08e82f7da684fb24b1f31ca548daf18d476
joshcoder2000 <jdd1993@hotmail.com>
2024-10-22 19:53:31 -0300

    Adds side navbar for small screens.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,32 +1,101 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import useWindowSize from "../hooks/windowSize";
-import { MdMenu } from "react-icons/md";
+import { MdHome, MdMenu, MdShoppingBag, MdShoppingCart } from "react-icons/md";
+import { IoMdInformationCircle } from "react-icons/io";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 export default function Nav() {
 	const windowSize = useWindowSize();
+	const navigate = useNavigate();
 	const isMobile = windowSize.windowWidth < 768;
+	const [isNavOpen, setIsNavOpen] = useState(false);
+
+	const NavShow = ({ children }: { children: JSX.Element }) => {
+		return (
+			<motion.div
+				initial={{ opacity: 0, x: 100 }}
+				whileInView={{ opacity: 1, x: 0 }}
+				viewport={{ once: true }}
+				transition={{ duration: 0.5 }}
+			>
+				{children}
+			</motion.div>
+		);
+	};
 
 	return (
-		<nav className='w-full text-white bg-black px-8 py-4 flex flex-row justify-end text-lg'>
-			<div className='mr-auto font-Anton'>Dreams Depot</div>
-
-			{isMobile && (
-				<MdMenu
-					size={24}
+		<header>
+			<nav className='w-full text-white bg-black px-8 py-4 flex flex-row justify-end text-lg'>
+				<div
 					onClick={() => {
-						console.log("clicked");
+						if (isNavOpen) {
+							setIsNavOpen(false);
+							navigate("/");
+						} else {
+							navigate("/");
+						}
 					}}
-				/>
-			)}
-
-			{!isMobile && (
-				<div className='flex flex-row gap-8'>
-					<Link to='/'>Home</Link>
-					<Link to='/shop'>Shop</Link>
-					<Link to='/cart'>Cart</Link>
-					<Link to='/about'>About</Link>
+					className={`${
+						isNavOpen && isMobile
+							? "ml-auto font-Anton text-xl"
+							: "mr-auto font-Anton"
+					}`}
+				>
+					Dreams Depot
 				</div>
-			)}
-		</nav>
+
+				{isMobile && !isNavOpen && (
+					<MdMenu
+						size={24}
+						onClick={() => {
+							setIsNavOpen(true);
+						}}
+					/>
+				)}
+
+				{!isMobile && (
+					<div className='flex flex-row gap-8'>
+						<Link to='/'>Home</Link>
+						<Link to='/shop'>Shop</Link>
+						<Link to='/cart'>Cart</Link>
+						<Link to='/about'>About</Link>
+					</div>
+				)}
+			</nav>
+			{/* Sidenav for small screens */}
+
+			<nav
+				className={`w-screen h-full absolute z-10  ${
+					isNavOpen && isMobile ? "bg-black/50" : "hidden"
+				}`}
+				onClick={() => {
+					setIsNavOpen(false);
+				}}
+			>
+				<NavShow>
+					<div className='min-w-[250px] w-6/12 h-screen bg-black absolute top-0 right-0 p-8'>
+						<div className='flex flex-col gap-8'>
+							<Link className='text-white flex gap-2' to='/'>
+								<MdHome size={21} />
+								Home
+							</Link>
+							<Link className='text-white flex gap-2' to='/shop'>
+								<MdShoppingBag size={21} />
+								Shop
+							</Link>
+							<Link className='text-white flex gap-2' to='/cart'>
+								<MdShoppingCart size={21} />
+								Cart
+							</Link>
+							<Link className='text-white flex gap-2' to='/about'>
+								<IoMdInformationCircle size={21} />
+								About
+							</Link>
+						</div>
+					</div>
+				</NavShow>
+			</nav>
+		</header>
 	);
 }
